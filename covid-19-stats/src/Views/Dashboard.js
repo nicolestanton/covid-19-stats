48951b9f85efedeaeeb62ed0bcbd8dc852e8aa01
Nicole Stanton <n.stanton14@ymail.com>
2021-04-15 14:46:57 +0100

    Add select component with locations

--- covid-19-stats/src/Views/Dashboard.js
+++ covid-19-stats/src/Views/Dashboard.js
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import axios from "axios";
 import useFetch from '../Hooks/useFetch'
+import Locations from '../Config/Locations'
+import Select from 'react-select'
 
 export function Dashboard() {
-  // const [data, setData] = useState();
-  const [location, setLocation] = useState('liverpool');
+  const [location, setLocation] = useState('England');
 
   const url = `https://api.coronavirus.data.gov.uk/v1/data?filters=areaName=${location}&structure={"date":"date","areaName":"areaName","areaCode":"areaCode","newCasesByPublishDate":"newCasesByPublishDate","cumCasesByPublishDate":"cumCasesByPublishDate","newDeathsByDeathDate":"newDeathsByDeathDate","cumDeathsByDeathDate":"cumDeathsByDeathDate"}`;
 
   const [data, status] = useFetch({ url, shouldExectute: location !== null });
 
-  console.log(data, status);
+  console.log('data:', data, 'status:', status);
+
+  if (status !== 'SUCCESS') {
+    return (
+      <div>Loading...</div>
+    )
+  }
 
   return (
     <React.Fragment>
-      <div className="App-header">
-        <h1>Covid-19 Information </h1>
-        <input
-          type="text"
-          placeholder="Enter Location"
-          value={location}
-          onChange={e => setLocation(e.target.value)}
-        />
-        {/* <button onClick={getData}>Get Api data</button> */}
+      <div className="container">
+        <h1>Covid-19 Information</h1>
+        <Select options={Locations} onChange={(e) => {
+          setLocation(e.value)
+        }} />
+        <h3>{location}</h3>
+        <h3>{data.data[0].date}</h3>
       </div>
     </React.Fragment>
   );
