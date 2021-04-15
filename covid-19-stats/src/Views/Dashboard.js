e23c7366dcf33162f7d1be78e18b11cf4f377d77
Nicole Stanton <n.stanton14@ymail.com>
2021-04-15 15:18:07 +0100

    add font awesome to project and get live stats showing

--- covid-19-stats/src/Views/Dashboard.js
+++ covid-19-stats/src/Views/Dashboard.js
@@ -4,10 +4,13 @@ import useFetch from '../Hooks/useFetch'
 import Locations from '../Config/Locations'
 import Select from 'react-select'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVirus, faBacteria } from '@fortawesome/free-solid-svg-icons'
+
 export function Dashboard() {
   const [location, setLocation] = useState('England');
 
-  const url = `https://api.coronavirus.data.gov.uk/v1/data?filters=areaName=${location}&structure={"date":"date","areaName":"areaName","areaCode":"areaCode","newCasesByPublishDate":"newCasesByPublishDate","cumCasesByPublishDate":"cumCasesByPublishDate","newDeathsByDeathDate":"newDeathsByDeathDate","cumDeathsByDeathDate":"cumDeathsByDeathDate"}`;
+  const url = `https://api.coronavirus.data.gov.uk/v1/data?filters=areaName=${location}&structure={"date":"date","areaName":"areaName","areaCode":"areaCode","newCases":"newCasesByPublishDate","totalCases":"cumCasesByPublishDate","newDeaths":"newDeathsByDeathDate","totalDeaths":"cumDeathsByDeathDate"}`;
 
   const [data, status] = useFetch({ url, shouldExectute: location !== null });
 
@@ -22,12 +25,19 @@ export function Dashboard() {
   return (
     <React.Fragment>
       <div className="container">
-        <h1>Covid-19 Information</h1>
-        <Select options={Locations} onChange={(e) => {
+        <h1>Covid-19 Information for {location}</h1>
+        <Select className='react-select' options={Locations} onChange={(e) => {
           setLocation(e.value)
         }} />
         <h3>{location}</h3>
         <h3>{data.data[0].date}</h3>
+        <FontAwesomeIcon icon={faBacteria} />
+        <h3>New Cases:{data.data[0].newCases}</h3>
+        <FontAwesomeIcon icon={faVirus} />
+        <h3>Total Cases:{data.data[0].totalCases}</h3>
+
+
+
       </div>
     </React.Fragment>
   );
